c1a5c83e4b547000515116b8a23a5564c4feab80
Matthew Reed <mattreed1890@gmail.com>
2014-08-07 23:10:26 -0500

    Add more user tests

--- models/User.php
+++ models/User.php
@@ -13,6 +13,7 @@ class User {
     private $username;
     private $email;
     private $password;
+    private $message;
 
     function __construct($username, $email, $password) {
         $this->username = trim($username);
@@ -23,16 +24,27 @@ class User {
     public function save() {
         // Validation
         if ($this->username == "") {
+            $this->message = "Please enter a username.";
             return false;
         } elseif ($this->email == "") {
+            $this->message = "Please enter an email.";
             return false;
         } elseif ($this->password == "") {
+            $this->message = "Please enter a password.";
             return false;
         } elseif (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $this->message = "Please enter a valid email.";
             return false;
+        } elseif (preg_match("/^[a-zA-Z0-9_-]{3,16}$/", $this->username) === 0) {
+            $this->message = "A username can consist of letters, numbers, dashes and underscores.";
+            return false;
+        } else {
+            // TODO Save to database
+            $this->message = "Account Created!";
+            return true;
         }
-        // TODO Save to database
-        return true;
+
+
     }
 
     public function setPassword($password) {
@@ -45,6 +57,10 @@ class User {
 
     public function setUsername($username) {
         $this->username = $username;
+    }
+
+    public function getMessage() {
+        return $this->message;
     }
 
     public static function find($i) {
--- models/UserTest.php
+++ models/UserTest.php
@@ -20,33 +20,42 @@ class UserTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(isset($this->user));
     }
 
+    function testValidCredentials() {
+        $this->assertTrue($this->user->save());
+        $this->assertEquals("Account Created!", $this->user->getMEssage());
+    }
+
     function testValidateUsernamePresence() {
         $this->user->setUsername("");
         $this->assertFalse($this->user->save());
+        $this->assertEquals("Please enter a username.", $this->user->getMEssage());
     }
 
     function testValidatePasswordPresence() {
         $this->user->setPassword("");
         $this->assertFalse($this->user->save());
+        $this->assertEquals("Please enter a password.", $this->user->getMEssage());
     }
 
     function testValidateEmailPresence() {
         $this->user->setEmail("");
         $this->assertFalse($this->user->save());
+        $this->assertEquals("Please enter an email.", $this->user->getMEssage());
     }
 
     function testValidateEmailFormat() {
         $this->user->setEmail("email");
         $this->assertFalse($this->user->save());
+        $this->assertEquals("Please enter a valid email.", $this->user->getMEssage());
     }
 
     function testValidateUsernameFormat() {
-        // TODO Implement Test
+        $this->user->setUsername("matt reed");
+        $this->assertFalse($this->user->save());
+        $this->assertEquals("A username can consist of letters, numbers, dashes and underscores.", $this->user->getMEssage());
     }
 
-    function testValidCredentials() {
-        $this->assertTrue($this->user->save());
-    }
+
 
     function tearDown() {
         unset($this->user);
