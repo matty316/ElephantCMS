a74d82bb0460fd1610bbb995dcb6597e2f03bd52
Matthew Reed <mattreed1890@gmail.com>
2014-08-07 23:27:16 -0500

    Move test to test folder

--- test/UserTest.php
+++ test/UserTest.php
@@ -6,8 +6,8 @@
  * Time: 9:37 PM
  */
 
-namespace models;
-require "User.php";
+require "../models/User.php";
+use models\User;
 
 class UserTest extends \PHPUnit_Framework_TestCase {
     private $user;
@@ -22,25 +22,25 @@ class UserTest extends \PHPUnit_Framework_TestCase {
 
     function testValidCredentials() {
         $this->assertTrue($this->user->save());
-        $this->assertEquals("Account Created!", $this->user->getMEssage());
+        $this->assertEquals("Account Created!", $this->user->getMessage());
     }
 
     function testValidateUsernamePresence() {
         $this->user->setUsername("");
         $this->assertFalse($this->user->save());
-        $this->assertEquals("Please enter a username.", $this->user->getMEssage());
+        $this->assertEquals("Please enter a username.", $this->user->getMessage());
     }
 
     function testValidatePasswordPresence() {
         $this->user->setPassword("");
         $this->assertFalse($this->user->save());
-        $this->assertEquals("Please enter a password.", $this->user->getMEssage());
+        $this->assertEquals("Please enter a password.", $this->user->getMessage());
     }
 
     function testValidateEmailPresence() {
         $this->user->setEmail("");
         $this->assertFalse($this->user->save());
-        $this->assertEquals("Please enter an email.", $this->user->getMEssage());
+        $this->assertEquals("Please enter an email.", $this->user->getMessage());
     }
 
     function testValidateEmailFormat() {
@@ -52,7 +52,7 @@ class UserTest extends \PHPUnit_Framework_TestCase {
     function testValidateUsernameFormat() {
         $this->user->setUsername("matt reed");
         $this->assertFalse($this->user->save());
-        $this->assertEquals("A username can consist of letters, numbers, dashes and underscores.", $this->user->getMEssage());
+        $this->assertEquals("A username can consist of letters, numbers, dashes and underscores.", $this->user->getMessage());
     }
 
     function testValidWithUppercaseUsername() {
